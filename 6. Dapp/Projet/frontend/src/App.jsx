5c9db38b4d855f36f78e5d3df7c2d21a71754675
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-11-14 16:32:28 +0100

    add connect wallet button

--- 6. Dapp/Projet/frontend/src/App.jsx
+++ 6. Dapp/Projet/frontend/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChakraProvider, Button } from '@chakra-ui/react';
+import { ethers } from 'ethers';
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const handleConnect = () => {
+    if (!window.ethereum) {
+      console.log('Install MetaMask');
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    provider.send('eth_requestAccounts', [])
+      .then((accounts) => {
+        if (accounts.length > 0) setCurrentAccount(accounts[0]);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const handleDisconnect = () => {
+    setCurrentAccount(undefined);
+  };
+
   return (
     <ChakraProvider>
-      <div className="text-3xl">Hello badisse</div>
-      <Button colorScheme="blue">Button</Button>
+      {
+        currentAccount
+          ? <Button colorScheme="blue" onClick={handleDisconnect}>{currentAccount}</Button>
+          : <Button colorScheme="blue" onClick={handleConnect}>Connect Wallet</Button>
+      }
     </ChakraProvider>
   );
 }
