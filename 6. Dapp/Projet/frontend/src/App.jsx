6f180534e9175b28e40d79c72461d80673dd38af
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-11-17 00:28:21 +0100

    remove chakra ui

--- 6. Dapp/Projet/frontend/src/App.jsx
+++ 6. Dapp/Projet/frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
 import { EthProvider } from './contexts/EthContext';
 import Home from './components/HomeLayout/Home';
 
 function App() {
   return (
-    <ChakraProvider>
-      <EthProvider>
-        <div className="bg-blue-800 text-white">
-          <Home />
-        </div>
-      </EthProvider>
-    </ChakraProvider>
+    <EthProvider>
+      <div className="bg-blue-800 text-white">
+        <Home />
+      </div>
+    </EthProvider>
   );
 }
 
