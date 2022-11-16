21cce054d90fd579189b2d0c513f15509dc5d367
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-11-17 00:28:48 +0100

    update button style

--- 6. Dapp/Projet/frontend/src/components/HomeLayout/Home.jsx
+++ 6. Dapp/Projet/frontend/src/components/HomeLayout/Home.jsx
@@ -26,12 +26,12 @@ function Home() {
       }
       <div className="h-screen flex flex-col items-center justify-center gap-10">
         <h1 className="text-5xl font-bold">Welcome To the Voting App</h1>
-        <h2 className="text-xl">This Web3 App allow you to create a voting session !</h2>
+        <h2 className="text-xl">This Web3 App allow you to create, manage or access a voting session !</h2>
         {
           !account
             ? (
               <button
-                className="bg-pink-800 p-5 font-semibold rounded-full animate-pulse"
+                className="bg-pink-800 p-5 font-semibold rounded-full animate-pulse hover:scale-125 transition ease-in-out duration-300"
                 type="button"
                 onClick={handleConnect}
               >
